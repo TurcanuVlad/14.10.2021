998a10a036980ade1c3f548bce0569dd47554fa4
Turcanu Vlad <70995414+TurcanuVlad@users.noreply.github.com>
2021-10-14 14:38:03 +0300

    Update matrice.py

--- matrice.py
+++ matrice.py
@@ -1,12 +1,21 @@
-a='A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
-b=''
-print(a.replace(' ', '-', len(a)))
-for i in range(0,len(a)):
-    if(a[i]!="Z" and a[i]!=" "):
-        b+=(chr(ord(a[i])+1))
-    else:
-        b+=a[i]
-print("literele:")
-print(b)
-print("z cu a:")
-print(a.replace("Z","A"))
+n=str(input("Introduceti sirul de caractere = "))
+a=n.isupper()
+ns=''
+ns2=''
+a='A'
+lin='-'
+n4=n
+from string import ascii_letters
+if a:
+for c in n4:
+if c in ascii_letters:
+ns=ns+ascii_letters[(ascii_letters.index(c)+1)%len(ascii_letters)]
+else:
+ns+=c
+ns=ns.replace("a","Z")
+print('a) Sirul de caractere inlocuit = ',ns)
+ns2=n.replace("Z","A")
+ns2=ns2.replace(" ","-")
+print('b,c) Stringul in care Z este inlocuit cu A si spatiul cu - = ',ns2)
+else:
+print('Eroare - sir de caractere inadmisibil')
